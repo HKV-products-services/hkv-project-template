3e78ffb6be5e57a5061cd418e356ec9dbfd9c89d
Marko Malis <markomalis@hotmail.com>
2018-11-12 16:53:23 +0100

    update index and add chart page example

---
+++ src/pages/chart.js
@@ -0,0 +1,3 @@
+export default () => (
+  <div>Hier een chart aub!</div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,17 @@
-export default () => <div>Welcome to a new HKV project!</div>
+import Link from 'next/link'
+
+export default () => (
+  <main>
+    <h1>HKV project template</h1>
+    <img src="/static/images/logo.png"/>
+    <div>
+      <Link href="/map">
+        <a>Kaart</a>
+      </Link>
+      <Link href="/chart">
+        <a>Vega</a>
+      </Link>
+    </div>
+  </main>
+
+)
